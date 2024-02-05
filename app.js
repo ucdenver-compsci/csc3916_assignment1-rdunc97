f2bb24ed433d3fdb531812e389c1769d1e184f76
RyanD <ryan.a.duncan@ucdenver.edu>
2024-02-04 19:00:45 -0700

    echo_server_try2

--- app.js
+++ app.js
@@ -1,31 +1,4 @@
-// var server = require("http").createServer();
 
-// server.on("request", (request, response) => {
-//     var body = [];
-//     request.on("data", chunk => {
-//         body.push(chunk);
-//     });
-//     request
-//         .on("end", () => {
-//             let bodyString = body.concat().toString();
-//             console.log(bodyString);
-//             response.end(bodyString);
-//         })
-//         .on("error", () => {
-//             response.statusCode = 400;
-//             response.end();
-//         });
-//     response.on("error", err => {
-//         console.error(err);
-//     });
-// });
-// server.listen(process.env.PORT || 8008, () => {
-//     console.log("Server listening at 8008");
-// });
-
-// module.exports = server; // for testing
-
-//curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
 
 var express = require('express');
 var app = express();
